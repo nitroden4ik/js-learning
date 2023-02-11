62a4cc8493f3e5114b5282a35b12757ccf2f82da
nitroden4ik <40860161+nitroden4ik@users.noreply.github.com>
2023-02-11 13:56:51 +0300

    6 tasks done

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ checkCharacter()
 
 */
 
-//Write a program that takes an input of a year, and outputs whether it is a leap year or not.
+/*Write a program that takes an input of a year, and outputs whether it is a leap year or not.
 
 
 function checkYear() {
@@ -135,6 +135,109 @@ function checkYear() {
 }
 
 alert(checkYear(userInput));
+*/
 
 
+/*Write a program that takes an input of a grade (A, B, C, D, or F) and outputs its equivalent numeric score (4 for A, 3 for B, 2 for C, 1 for D, and 0 for F).
+const gradesAlphabet = [  'A',  'B',  'C',  'D',  'F',];
+const gradesNumeric = [  4,  3,  2,  1,  0,];
 
+let userInput = prompt('Enter your grade');
+
+let index = gradesAlphabet.indexOf(userInput.toUpperCase());
+
+if (index !== -1){
+  alert(`your numeric score is  ${gradesNumeric[index]}`)
+} else {
+  alert('not a grade')
+}
+
+*/
+
+/*Write a function that takes a string as an argument and returns "Yes" if the string is not empty, and "No" otherwise.
+
+let userInput = prompt('Enter anything');
+
+if (userInput.length > 0){
+  alert('Yes')
+} else {
+  alert('No')
+}
+*/
+
+/*Write a function that takes a temperature in Celsius as an argument and returns the equivalent temperature in Fahrenheit.
+
+let userInput = prompt('Enter temperature in Celsius');
+
+result = (Number(userInput) * 9/5) + 32;
+if(!Number(userInput)){
+  alert('enter a number!')
+} else {
+  alert(result)
+}
+
+*/
+
+/*Write a function that takes a number as an argument and returns "Even" if the number is even, and "Odd" otherwise.
+let userInput = prompt('Enter a number');
+
+(function (){
+  return Number(userInput) % 2 === 0 ? alert('it is even number') : alert('it is odd and weird');
+
+}
+)();
+*/
+
+/*
+---------------------------------------------------------------
+Arrays and Array methods like map, filter, reduce, etc.
+---------------------------------------------------------------
+lets go
+*/
+/* Create an array of numbers and use the map method to square each number in the array.
+
+let numbers = [1, 2, 3, 4, 5];
+
+
+//for(i = 0; i < numbers.length; i ++){
+  
+  let squaredNumbers = numbers.map(num => num * num);
+  
+    console.log(squaredNumbers)
+}
+*/
+
+
+  /*Create an array of objects representing books and use the filter method to find all books with a certain author.
+const books = [
+{name: 'book 1',
+author: 'Gogol',},
+
+{name: 'book 2',
+author: 'Tolstoy',},
+
+{name: 'book 3',
+author: 'Dostoevsky',},
+
+{name: 'book 4',
+author: 'Dostoevsky',},
+
+]
+let searchAuthor = "Tolstoy";
+let authorBooks = books.filter(book => book.author === searchAuthor);
+console.log(authorBooks);
+
+*/
+
+/*Create an array of numbers and use the reduce method to find the sum of all the numbers.
+
+const numbers = [1, 2, 3, 4, 5];
+const numbers2 = [5, 5, 5, 5, 5];
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+console.log(sum); // Output: 15
+
+const sum2 = numbers2.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+
+console.log(sum2); // Output: 25
+*/
